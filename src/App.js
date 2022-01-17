390327f1ba4d86652badac527cb29aea3d43ab25
simon <scriado.f@gmail.com>
2022-01-17 17:37:54 +0100

    Added conditional navbar links, user avatars, online users, create project form, react select

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Signup from './pages/signup/Signup'
 import Create from './pages/create/Create'
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import OnlineUsers from './components/OnlineUsers';
 
 function App() {
   const { user, authIsReady } = useAuthContext()
@@ -20,7 +21,7 @@ function App() {
     <div className="App">
       {authIsReady && (
         <BrowserRouter>
-          <Sidebar />
+          {user && <Sidebar />}
           <div className="container">
             <Navbar />
             <Switch>
@@ -46,6 +47,7 @@ function App() {
               </Route>
             </Switch>
           </div>
+          {user && <OnlineUsers />}
         </BrowserRouter>
       )}
     </div>
