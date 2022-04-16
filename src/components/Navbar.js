acfbbf629054c5894ab7365cbb64e29691fc556d
simon <scriado.f@gmail.com>
2022-04-16 12:30:22 +0200

    Adding comments & notes to the project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,47 @@
-import { Link } from 'react-router-dom'
-import { useLogout } from '../hooks/useLogout'
+import { Link } from 'react-router-dom';
+import { useLogout } from '../hooks/useLogout';
 import { useAuthContext } from '../hooks/useAuthContext';
 
 // Styles & images
-import './Navbar.css'
-import House from '../assets/ms-icon-144x144.png'
+import './Navbar.css';
+import House from '../assets/ms-icon-144x144.png';
 
 export default function Navbar() {
-    const { logout, isPending } = useLogout()
-    const { user } = useAuthContext()
+  const { logout, isPending } = useLogout();
+  const { user } = useAuthContext();
 
-    return (
-        <div className='navbar'>
-            <ul>
-                <li className="logo">
-                    <img src={House} alt="dojo logo" />
-                    <span>Lita Farigola</span>
-                </li>
-                {!user && (
-                    <>
-                        <li><Link to="/login">Login</Link></li>
-                        <li><Link to="/signup">Signup</Link></li>
-                    </>
-                )}
-                {user && (
-                    <li>
-                        {!isPending && <button className="btn" onClick={logout}>Logout</button>}
-                        {isPending && <button className="btn" disabled>Logging out...</button>}
-                    </li>
-                )}
-            </ul>
-        </div>
-    )
+  return (
+    <div className='navbar'>
+      <ul>
+        <li className='logo'>
+          <img src={House} alt='dojo logo' />
+          <span>Home Tasks Manager</span>
+        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+            <li>
+              <Link to='/signup'>Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <li>
+            {!isPending && (
+              <button className='btn' onClick={logout}>
+                Logout
+              </button>
+            )}
+            {isPending && (
+              <button className='btn' disabled>
+                Logging out...
+              </button>
+            )}
+          </li>
+        )}
+      </ul>
+    </div>
+  );
 }
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,26 +1,27 @@
-import firebase from "firebase/app";
-import 'firebase/firestore'
-import 'firebase/auth'
-import 'firebase/storage'
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/storage';
 
+// We copy this from Firebase, when creating the new project
 const firebaseConfig = {
-    apiKey: "AIzaSyD42NaDko8kSDozXRHS1Lz-L_2_DQW0S8M",
-    authDomain: "thedojosite-fe490.firebaseapp.com",
-    projectId: "thedojosite-fe490",
-    storageBucket: "thedojosite-fe490.appspot.com",
-    messagingSenderId: "620817143562",
-    appId: "1:620817143562:web:282e0aa5bf5b8fbcd08cf3"
+  apiKey: 'AIzaSyD42NaDko8kSDozXRHS1Lz-L_2_DQW0S8M',
+  authDomain: 'thedojosite-fe490.firebaseapp.com',
+  projectId: 'thedojosite-fe490',
+  storageBucket: 'thedojosite-fe490.appspot.com',
+  messagingSenderId: '620817143562',
+  appId: '1:620817143562:web:282e0aa5bf5b8fbcd08cf3',
 };
 
 // Init firebase
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
 // Init services
-const projectFirestore = firebase.firestore()
-const projectAuth = firebase.auth()
-const projectStorage = firebase.storage()
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+const projectStorage = firebase.storage();
 
 // Timestamp
-const timestamp = firebase.firestore.Timestamp
+const timestamp = firebase.firestore.Timestamp;
 
-export { projectFirestore, projectAuth, projectStorage, timestamp }
+export { projectFirestore, projectAuth, projectStorage, timestamp };
